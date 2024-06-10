048a156958c5cb83db67a92b5d322308a32a4d6e
mfceballosc <mfceballosc@unal.edu.co>
2024-06-10 00:12:04 -0500

    Actualizacion de script, sin bd

--- 04-trabajo-final/01_gen_data/proporcionComunas.py
+++ 04-trabajo-final/01_gen_data/proporcionComunas.py
@@ -17,75 +17,80 @@ import matplotlib.pyplot as plt
 from shapely import wkb
 from shapely.geometry import Point
 
-from src import leer_archivo_comuna, calcular_data, gen_datetime, asociar_comunas
+
+from src import leer_archivo_comuna, calcular_data, gen_datetime, asociar_comunas, gen_data, generar_coordenadas
 
 file_path = r"data\1. Proyecciones Medellín por Comunas y Corregimientos 2018 - 2030.xlsx"
 
 file_comunas_path = r"../base.data/medellin_neighborhoods.parquet"
-file_res_path = r"../base.data/datos_por_comuna.parquet"
-
-import numpy as np
-from shapely.geometry import Point
-
-def gen_data(data):
-    geometry = shapely.wkb.loads(data['geometry'])[0]
-    x0, y0, x1, y1 = geometry.bounds
-    n_data = list(data.Ponerado)[0]
-    points = []
-    while len(points) < n_data:
-        point = Point(np.random.uniform(x0, x1), np.random.uniform(y0, y1))
-        if point.within(geometry):
-            # points.append(point)
-            points.append(wkb.dumps(point))
-    n = len(points)
-    base = data.iloc[0]
-    n_base = pd.concat([base]*(n-1), axis=1).transpose()
-    df = pd.concat([data, n_base], ignore_index=True)
-    df['points'] = points
-    df['geometry'] = list(data['geometry'])[0]
-    return df
+file_res_path = r"../base.data/datos_por_comuna2.parquet"
+file_customers = r'../base.data/customers.parquet'
+file_employees = r'../base.data/employees.parquet'
 
 
-def generar_coordenadas(df):
-    df_res = pd.DataFrame()
-    comunas = list(df.NOMBRE)
-    df['puntos'] = ''
-    df['FECHA'] = ''
-    for comuna in comunas:
-        if comuna==None:
-            continue
-        msk = df.NOMBRE==comuna
-        d_comuna = df.loc[msk]
-        df_comuna = gen_data(d_comuna)
-        # if df_res.shape[0] > 0:
-        df_res = pd.concat([df_res, df_comuna], ignore_index=True)
-    
-    cols = ['OBJECTID', 'CODIGO', 'NOMBRE', 'IDENTIFICACION', 'LIMITEMUNICIPIOID',
-            'SUBTIPO_COMUNACORREGIMIENTO', 'LINK_DOCUMENTO', 'SHAPEAREA', 'FECHA',
-            'SHAPELEN', 'Ponerado', 'puntos', 'points', 'geometry']
-    df_res = df_res[cols]
+def asociar_id_elm1_elm2(df, df_comuna, tipo):
+    """
+    asocia dos dataframes, elemento1 y elemento 2.
+    Pueden ser usuarios y comunas, se crea las columnas para la union.
+    Pueden ser empleados y comunas, se crea las columnas para la union.
+    """
+    new_col = 'ID_aux'
+    # renombrar las columnas segun el tipo
+    cols = {col:f"{tipo}_{col}" for col in df.columns}
+    df.rename(columns=cols, inplace=True)
+    n_data = df_comuna.shape[0]
+    array = np.random.uniform(0, df.shape[0], n_data)
+    array = list(map(int, array))
+    n_cust = [i for i in range(df.shape[0])]
+    df[new_col] = n_cust
+    df_comuna[new_col] = array
+    df_res = pd.merge(df_comuna, df, on=new_col, how='inner')
+    df_res.drop(new_col, axis=1, inplace=True)
     return df_res
+
+
 
 if __name__ == "__main__":
     # np.random.seed(43)
-    fecha = "03/06/2024"
-    fecha = datetime.strptime(fecha, "%d/%m/%Y")  
+    fecha = "03/04/2024"
+    n_dias = 2
     
-    df_i = leer_archivo_comuna(file_path)
+    fecha = datetime.strptime(fecha, "%d/%m/%Y")
+    f_temp = [fecha + timedelta(days=d) for d in range(n_dias)]
     
-    # porcentaje, definimos un 5%
-    n_datos = 1000
-    df_com_base = calcular_data(df_i, n_datos)
+    fechas = [datetime.strptime(f.strftime("%Y/%m/%d"), "%Y/%m/%d") for f in f_temp]    
 
-    df_comunas = pd.read_parquet(file_comunas_path)    
-    df = asociar_comunas(df_comunas, df_com_base)
+    df_parquet = pd.DataFrame()
     
-    # cargamos la geometria
-    df = generar_coordenadas(df)
-    l_fecha = gen_datetime(fecha, df.shape[0])
-    df['FECHA'] = l_fecha
-    
-    
-    df.to_parquet(file_res_path)
+    for cont, fecha in enumerate(fechas):
+        print('*'.center(50, '*'))
+        print(cont)
+        print('*'.center(50, '*'))
+        n_datos = 1000
+        
+        df_i = leer_archivo_comuna(file_path)
+        df_cust = pd.read_parquet(file_customers)
+        df_empl = pd.read_parquet(file_employees)
+        df_comunas = pd.read_parquet(file_comunas_path) 
+        
 
+        df_com_base = calcular_data(df_i, n_datos)
+    
+        # df_comunas = pd.read_parquet(file_comunas_path)    
+        df = asociar_comunas(df_comunas, df_com_base)
+        
+        df = generar_coordenadas(df)
+        l_fecha = gen_datetime(fecha, df.shape[0])
+        df['FECHA'] = l_fecha    
+        
+        # df_cust = pd.read_parquet(file_customers)
+        df = asociar_id_elm1_elm2(df_cust, df, 'customer')
+        
+        # df_empl = pd.read_parquet(file_employees)
+        df = asociar_id_elm1_elm2(df_empl, df, 'employee')  
+        
+        df_parquet = pd.concat([df_parquet, df], ignore_index=True)
+    
+    df_parquet.to_parquet(file_res_path)
 
+    
--- 04-trabajo-final/01_gen_data/src/common.py
+++ 04-trabajo-final/01_gen_data/src/common.py
@@ -1,6 +1,12 @@
 from datetime import datetime, timedelta
 import pandas as pd
 import numpy as np
+from shapely.geometry import Point
+import shapely.wkb
+import geopandas as gpd
+import matplotlib.pyplot as plt
+from shapely import wkb
+
 
 def leer_archivo_comuna(path, anno = 2024):    
     df = pd.read_excel(path, skiprows=15, nrows=23)
@@ -24,7 +30,6 @@ def calcular_data(df, size=1000):
     pesos = df.Proporcion.tolist()
     df['Ponderados'] = [int(np.random.uniform(prop*3/4, prop+prop/2)*size) for prop in pesos]
     # df['Ponderados'] = [int(np.random.uniform(prop*3/4, prop+prop/2) * size) if prop == 0 else 1 for prop in pesos]
-
     return df
 
 
@@ -51,6 +56,7 @@ def gen_datetime(fecha, cantidad):
     res = map(f, res)    
     return list(res)
 
+
 def asociar_comunas(df1, df2):   
     cols_d1 = list(df1.NOMBRE)
     cols_d2 = list(df2.Comuna)    
@@ -66,3 +72,44 @@ def asociar_comunas(df1, df2):
                 msk = df1.NOMBRE==c1                
                 df1.loc[msk, 'Ponerado'] = val
     return df1
+
+
+def gen_data(data):
+    geometry = shapely.wkb.loads(data['geometry'])[0]
+    x0, y0, x1, y1 = geometry.bounds
+    n_data = list(data.Ponerado)[0]
+    points = []
+    while len(points) < n_data:
+        point = Point(np.random.uniform(x0, x1), np.random.uniform(y0, y1))
+        if point.within(geometry):
+            # points.append(point)
+            points.append(wkb.dumps(point))
+    n = len(points)
+    base = data.iloc[0]
+    n_base = pd.concat([base]*(n-1), axis=1).transpose()
+    df = pd.concat([data, n_base], ignore_index=True)
+    df['points'] = points
+    df['geometry'] = list(data['geometry'])[0]
+    return df
+
+
+def generar_coordenadas(df):
+    df_res = pd.DataFrame()
+    comunas = list(df.NOMBRE)
+    df['puntos'] = ''
+    df['FECHA'] = ''
+    for comuna in comunas:
+        if comuna==None:
+            continue
+        msk = df.NOMBRE==comuna
+        d_comuna = df.loc[msk]
+        df_comuna = gen_data(d_comuna)
+        # if df_res.shape[0] > 0:
+        df_res = pd.concat([df_res, df_comuna], ignore_index=True)    
+    cols = ['OBJECTID', 'CODIGO', 'NOMBRE', 'IDENTIFICACION', 'LIMITEMUNICIPIOID',
+            'SUBTIPO_COMUNACORREGIMIENTO', 'LINK_DOCUMENTO', 'SHAPEAREA', 'FECHA',
+            'SHAPELEN', 'Ponerado', 'puntos', 'points', 'geometry']
+    df_res = df_res[cols]
+    return df_res
+
+
